e13cf1590160083d5d8552b2d3697ea5a019e8e8
Benson Peng <penglingzhe@gmail.com>
2013-01-03 18:59:53 +0800

    add JSONUtil

--- src/com/orange/common/android/utils/JsonUtil.java
+++ src/com/orange/common/android/utils/JsonUtil.java
@@ -5,13 +5,37 @@ import org.json.JSONObject;
 
 public class JsonUtil {
 
-	public static String getStringOrNull(JSONObject json, String key) {
+	public static String getString(JSONObject json, String key) {
 		try {
 			if (json == null || json.isNull(key)) {
 				return null;
 			}
 
 			return json.getString(key);
+		} catch (JSONException e) {
+			return null;
+		}
+	}
+	
+	public static int getInt(JSONObject json, String key) {
+		try {
+			if (json == null || json.isNull(key)) {
+				return 0;
+			}
+
+			return json.getInt(key);
+		} catch (JSONException e) {
+			return 0;
+		}
+	}
+	
+	public static Object get(JSONObject json, String key) {
+		try {
+			if (json == null || json.isNull(key)) {
+				return null;
+			}
+
+			return json.get(key);
 		} catch (JSONException e) {
 			return null;
 		}

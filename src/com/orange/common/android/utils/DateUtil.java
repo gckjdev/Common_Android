78a2dcf47d38a799134b6992fba0f22fcc6024ee
liuxiaokun <lk674993264@gmail.com>
2013-01-12 09:56:53 +0800

    add dateutil

---
+++ src/com/orange/common/android/utils/DateUtil.java
@@ -0,0 +1,33 @@
+/**  
+        * @title DateUtil.java  
+        * @package com.orange.common.android.utils  
+        * @description   
+        * @author liuxiaokun  
+        * @update 2013-1-9 下午4:58:51  
+        * @version V1.0  
+ */
+package com.orange.common.android.utils;
+
+import android.util.Log;
+
+
+
+public class DateUtil
+{
+
+	private static final String TAG = "DateUtil";
+
+	public static String dateFormatToString(int date){
+		String dateString = "";
+		long currentTime = System.currentTimeMillis()/1000;
+		Log.d(TAG, "current time = "+currentTime);
+		Log.d(TAG, "draw date = "+date);
+		date = (int)currentTime-date;
+		if(date/60>60){
+			dateString = date/3600+"小时前";
+		}else {
+			dateString = (date/60<1?1:date/60) +"分钟前";
+		}
+		return dateString;
+	}
+}
